993e3930aa05fd99c6e04b5b3f046a47a4855d92
Daniel Garcia <61481194+danny-garxia@users.noreply.github.com>
2023-04-22 08:48:44 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React , {useState, useEffect}from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Dashboard, Home, LogIn, SignUp } from './pages';
+import { Post } from './components';
+import { db } from './config/firebase';
 
-function App() {
-  return (
-    <main>
-      <Routes>
+
+/*
+ <Routes>
         <Route path="/">
           <Route index element={<Home />} />
           <Route path="sign-up" element={<SignUp />} />
@@ -15,8 +16,39 @@ function App() {
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="*" element={<p>We can't find that page!</p>} />
         </Route>
-      </Routes>
-    </main>
+      </Routes>*/
+
+function App() {
+  const [posts,setPosts] = useState([]);
+  /// USe effect to add consitions 
+
+  useEffect(() => {
+    // This is where the code runs
+    db.collection('posts').onSnapshot(snapshot => {
+      // every time a new post is added, this code fires up
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
+    })
+}, []);
+  return (
+  <div className= "app">
+    <div className="app__header">
+      <img
+      className="app_headerImage"
+      src = "public/estatelogo.png"
+      />
+    </div>
+   
+    {
+      posts.map(posts =>(
+        <Post username={posts.username} caption={posts.caption} imageUrl={posts.imageUrl}/>
+      ))
+    }
+  </div>
+
+  
   );
 }
 
